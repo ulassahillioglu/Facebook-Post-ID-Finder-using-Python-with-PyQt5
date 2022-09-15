800fc3b82785c9f7430b346f6798cbd9dc3fb02b
ulassahillioglu <101193477+ulassahillioglu@users.noreply.github.com>
2022-09-15 16:07:50 +0300

    Create embedpost.py

---
+++ embedpost.py
@@ -0,0 +1,10 @@
+link = input("Enter the link : ")
+link = str(input_link).replace("www","m")
+res = rq.get(link)
+soup_data = bs(res.text, 'html.parser')
+concl = soup_data.find_all('div',{'class' : 'bo bp'})
+
+s = str(concl[0]).split(",")
+global final_result
+final_result = s[1]
+print(final_result)
