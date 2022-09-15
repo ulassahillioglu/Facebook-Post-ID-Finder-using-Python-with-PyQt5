e07cc41db71c2cc6528bf0e182984aac457f64ea
ulassahillioglu <101193477+ulassahillioglu@users.noreply.github.com>
2022-09-15 16:09:35 +0300

    Create textPost.py

---
+++ textPost.py
@@ -0,0 +1,10 @@
+input_link = input("Enter your link : ")
+link = str(input_link).replace("www","m")
+res = rq.get(link)
+soup_data = bs(res.text, 'html.parser')
+concl = soup_data.find_all('div',{'class' : 'bo bp'})
+
+s = str(concl[0]).split(",")
+global final_result
+final_result = s[2]
+print(final_result)
